f2a1240ba42af3e10cfc2fe122498fcc62817a25
bossbotDev <52401739+bossbotDev@users.noreply.github.com>
2019-07-02 14:40:01 +0900

    Add files via upload

---
+++ MainBot.py
@@ -0,0 +1,26 @@
+
+# 보스 로봇 마이그레이선 v1.0
+
+import asyncio 
+import discord 
+from discord.ext.commands import Bot 
+
+
+class BossBot(discord.Client):
+    def __init_(self):
+        self.aiolocks = defaultdict(asyncio.Lock)
+        self.voice_client_connectLock = asyncio.Lock()
+        self.voice_client_moveLock = asyncio.Lock()
+        
+        
+async def AutoJoin_Channels(self, channels):
+    join_servers_ed = []
+    
+    for channels in channels:
+        if channels.server in join_servers_ed:
+            #서버 출력부....
+            continue
+            
+            
+        
+        
